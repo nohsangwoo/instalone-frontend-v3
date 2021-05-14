3e79e98f093df0513bff58a402a7d2a6a1422d1f
nohsangwoo <nsgr12@naver.com>
2021-05-14 16:20:56 +0900

    #8.7 Introduction to Styled Components

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,11 +1,45 @@
-import { isLoggedInVar } from '../apollo';
+import { useState } from 'react';
+import styled, { css } from 'styled-components';
+
+// import { isLoggedInVar } from '../apollo';
+
+type TitleStyleProps = {
+  potato: boolean;
+};
+const Title = styled.h1<TitleStyleProps>`
+  color: ${props => (props.potato ? 'palevioletred' : 'beige')};
+  font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  ${props =>
+    props.potato
+      ? css`
+          font-size: 49px;
+        `
+      : css`
+          text-decoration: underline;
+        `}
+`;
+
+const Container = styled.div`
+  background-color: tomato;
+`;
+
+const TogglePotato = styled.button`
+  color: red;
+`;
 
 function Login() {
+  const [potato, setPotato] = useState(false);
+  const togglePotato = () =>
+    setPotato(current => {
+      console.log('current확인: ', current);
+      return !current;
+    });
   return (
-    <div>
-      <h1>Plz. Login</h1>
-      <button onClick={() => isLoggedInVar(true)}>Log in Now!</button>
-    </div>
+    <Container>
+      <Title potato={potato}>Login</Title>
+      <TogglePotato onClick={togglePotato}>Toggle Potato</TogglePotato>
+    </Container>
   );
 }
 
