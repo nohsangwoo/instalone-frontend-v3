4451832ab9b4a5c773f2c252240074b6c554e7b5
nohsangwoo <nsgr12@naver.com>
2021-05-14 14:31:00 +0900

    apply typescript for useState

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 type Props = {
   setIsLoggedIn: (args: boolean) => void;
 };
-function Home(props: Props) {
-  return <div>Home</div>;
+function Home({ setIsLoggedIn }: Props) {
+  return (
+    <div>
+      <h1>Home</h1>
+      <button onClick={() => setIsLoggedIn(false)}>Log out Now!</button>
+    </div>
+  );
 }
 
 export default Home;
--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -4,8 +4,13 @@ type Props = {
   setIsLoggedIn: (args: boolean) => void;
 };
 
-function Login(props: Props) {
-  return <div>Plz. Login</div>;
+function Login({ setIsLoggedIn }: Props) {
+  return (
+    <div>
+      <h1>Plz. Login</h1>
+      <button onClick={() => setIsLoggedIn(true)}>Log in Now!</button>
+    </div>
+  );
 }
 
 export default Login;
