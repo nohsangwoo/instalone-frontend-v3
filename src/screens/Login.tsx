7828420996eafaf52a1b34ef374bc8644edb4451
nohsangwoo <nsgr12@naver.com>
2021-05-18 16:37:59 +0900

    invalid type 지정

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -25,43 +25,17 @@ const FacebookLogin = styled.div`
 // Types to be entered in form
 // also Data type when invalid
 type FormData = {
-  username?: string;
-  password?: string;
-};
-
-// Data type when valid
-type FieldError = {
-  FormData: {
-    type: 'minLength' | 'required';
-    ref: HTMLInputElement;
-    message: string;
-  };
+  username: string;
+  password: string;
 };
 
 const Login = (): JSX.Element => {
-  // typescript 적용 예시
-  // const [username, setUsername] = useState('');
-  // const [usernameError, setUsernameError] = useState('');
-  // const onUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-  //   setUsernameError('');
-  //   setUsername(event.target.value);
-  // };
-  // const handleSubmit = (event: React.FormEvent) => {
-  //   event.preventDefault();
-  //   if (username === '') {
-  //     setUsernameError('Not empty pls.');
-  //   }
-  //   if (username.length < 10) {
-  //     setUsernameError('too short');
-  //   }
-  // };
-
   const { register, handleSubmit } = useForm<FormData>();
-  const onSubmitValid = (data: FieldError): void => {
+  const onSubmitValid = (data: FormData): void => {
     console.log('data: ', data);
   };
 
-  const onSubmitInvalid = (data: any): void => {
+  const onSubmitInvalid = (data: Record<string, any>): void => {
     console.log(data, 'invalid');
   };
   return (
@@ -71,14 +45,6 @@ const Login = (): JSX.Element => {
         <div>
           <FontAwesomeIcon icon={faInstagram} size="3x" />
         </div>
-        {/* <form onSubmit={handleSubmit}>
-          {usernameError}
-          <Input
-            onChange={onUsernameChange}
-            value={username}
-            type="text"
-            placeholder="Username"
-          /> */}
 
         {/* handleSubmit의 첫번째 인자는 입력의 조건에 맞을때, 
         두번째 인자는 조건에 맞지 않는 입력값을 받았을때 */}
