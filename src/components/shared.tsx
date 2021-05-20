bb29a7faefb1a2eec8ccb099436ee13bc1c0db4c
nohsangwoo <nsgr12@naver.com>
2021-05-20 17:35:47 +0900

    #11.4 Photo Component part One

--- src/components/shared.tsx
+++ src/components/shared.tsx
@@ -15,3 +15,7 @@ export const FatLink = styled.span`
   font-weight: 600;
   color: rgb(142, 142, 142);
 `;
+
+export const FatText = styled.span`
+  font-weight: 600;
+`;
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,73 @@
+import { gql, useQuery } from '@apollo/client';
+import styled from 'styled-components';
 import { logUserOut } from '../apollo';
 
+import Avatar from '../components/Avatar';
+import { FatText } from '../components/shared';
+
+// 나를 follow하는 모든 user의 feed와 && 나의 feed를 전부 로드하여 불러온다
+// 위 내용의 트리거
+const FEED_QUERY = gql`
+  query seeFeed {
+    seeFeed {
+      id
+      user {
+        username
+        avatar
+      }
+      file
+      caption
+      likes
+      comments
+      createdAt
+      isMine
+    }
+  }
+`;
+
+const PhotoContainer = styled.div`
+  background-color: white;
+  border: 1px solid ${props => props.theme.borderColor};
+  margin-bottom: 20px;
+`;
+const PhotoHeader = styled.div`
+  padding: 5px 10px;
+  display: flex;
+  align-items: center;
+`;
+
+const Username = styled(FatText)`
+  margin-left: 5px;
+`;
+
 function Home(): JSX.Element {
+  // 피드를 불러온다.
+  const { data } = useQuery(FEED_QUERY);
+
+  // for seeFeed's typescript
+  type returnValue = {
+    id: string;
+    user: {
+      username: string;
+      avatar: string;
+    };
+    file: string;
+    caption: string;
+    likes: string;
+    comments?: string;
+    createdAt: string;
+    isMine: boolean;
+  };
   return (
     <div>
-      <h1>Welcome we did it!</h1>
-      <button onClick={() => logUserOut()}>Log out now!</button>
+      {data?.seeFeed?.map((photo: returnValue) => (
+        <PhotoContainer key={photo.id}>
+          <PhotoHeader>
+            <Avatar url={photo.user.avatar} />
+            <Username>{photo.user.username}</Username>
+          </PhotoHeader>
+        </PhotoContainer>
+      ))}
     </div>
   );
 }
