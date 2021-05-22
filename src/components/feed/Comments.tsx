29235255c39456ea97c3cf8805aeeed9536f5ca2
nohsangwoo <nsgr12@naver.com>
2021-05-22 13:23:20 +0900

    #11.16 Create Comment part One

--- src/components/feed/Comments.tsx
+++ src/components/feed/Comments.tsx
@@ -1,5 +1,17 @@
 import styled from 'styled-components';
 import Comment from './Comment';
+import { useForm } from 'react-hook-form';
+import { gql, useMutation } from '@apollo/client';
+
+// useMutation을 위한 트리거 생성
+const CREATE_COMMENT_MUTATION = gql`
+  mutation createComment($photoId: Int!, $payload: String!) {
+    createComment(photoId: $photoId, payload: $payload) {
+      ok
+      error
+    }
+  }
+`;
 
 const CommentsContainer = styled.div`
   margin-top: 20px;
@@ -13,6 +25,7 @@ const CommentCount = styled.span`
 `;
 
 type Props = {
+  photoId: number;
   author: string;
   caption: string;
   commentNumber?: number;
@@ -28,7 +41,34 @@ type Props = {
   }[];
 };
 
-function Comments({ author, caption, commentNumber, comments }: Props) {
+function Comments({
+  photoId,
+  author,
+  caption,
+  commentNumber,
+  comments,
+}: Props) {
+  // mutation hook 생성
+  const [createCommentMutation, { loading }] = useMutation(
+    CREATE_COMMENT_MUTATION
+  );
+  const { register, handleSubmit, setValue } = useForm();
+  //  form 에서 submit시에 전달 하려는 props의 값들
+  const onValid = (data: { payload: string }) => {
+    const { payload } = data;
+    // useMutation 실행중 중복 클릭을 방지하기위한 안전장치
+    if (loading) {
+      return;
+    }
+    // mutatio hook을 실질적으로 실행하는 위치
+    createCommentMutation({
+      variables: {
+        photoId,
+        payload,
+      },
+    });
+    setValue('payload', '');
+  };
   return (
     <CommentsContainer>
       <Comment author={author} payload={caption} />
@@ -42,6 +82,15 @@ function Comments({ author, caption, commentNumber, comments }: Props) {
           payload={comment.payload}
         />
       ))}
+      <div>
+        <form onSubmit={handleSubmit(onValid)}>
+          <input
+            {...register('payload', { required: true })}
+            type="text"
+            placeholder="Write a comment..."
+          />
+        </form>
+      </div>
     </CommentsContainer>
   );
 }
