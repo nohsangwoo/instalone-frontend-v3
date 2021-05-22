8b0d31e0ed979d3189ba4c350034d2b05a817406
nohsangwoo <nsgr12@naver.com>
2021-05-22 12:50:27 +0900

    # 11.14 Parsing Hashtags part Two

--- src/components/feed/Comment.tsx
+++ src/components/feed/Comment.tsx
@@ -31,11 +31,15 @@ function Comment({ author, payload }: Props) {
       <FatText>{author}</FatText>
       <CommentCaption>
         {payload.split(' ').map((word, index) =>
+          // 해당 정규표현 식이   맞는지 테스트후 true or false 로 반환한다.
           /#[\w]+/.test(word) ? (
+            // true로 반환 받은 word인 경우
+            // Link태그를 사용하여 해당 route로 이동하게 만들어주게 한다
             <React.Fragment key={index}>
               <Link to={`/hashtags/${word}`}>{word}</Link>
             </React.Fragment>
           ) : (
+            // false로 반환 받은 word인 경우 아래의 조건으로 반환
             <React.Fragment key={index}>{word} </React.Fragment>
           )
         )}
